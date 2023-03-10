b6f4b6d81ca7ff6df7b7e332c53f7093750986f9
newbeedaly <newbeedaly@163.com>
2023-03-10 10:44:40 +0800

    springboot init

--- pay-domain/src/main/java/cn/newbeedaly/pay/PaySpringApplication.java
+++ pay-domain/src/main/java/cn/newbeedaly/pay/PaySpringApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DubboPayApplication {
+public class PaySpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DubboPayApplication.class, args);
+        SpringApplication.run(PaySpringApplication.class, args);
     }
-
 }
